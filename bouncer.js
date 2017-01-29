32c2f31565771462de77b6bc3fcec7d322fb4019
Chigoziem Nwaiwu <chigodwin20@yahoo.com>
2017-01-29 15:06:58 +0100

    Update bouncer.js
    Easy to understand

--- bouncer.js
+++ bouncer.js
@@ -3,6 +3,8 @@ function bouncer(arr) {
   // Don't show a false ID to this bouncer.
   var newArr = [];
   var g, h;
+  
+//   FOR LOOP HERE
   for(var i = 0; i < arr.length; i++) {
     h = arr[i];
     g = Boolean(h);
