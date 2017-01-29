316623cf5786c5f6714b42bfcaced052507edd32
Chigoziem Nwaiwu <chigodwin20@yahoo.com>
2017-01-29 14:35:12 +0100

    Create bouncer.js

---
+++ bouncer.js
@@ -0,0 +1,23 @@
+
+function bouncer(arr) {
+  // Don't show a false ID to this bouncer.
+  var newArr = [];
+  var g, h;
+  for(var i = 0; i < arr.length; i++) {
+    h = arr[i];
+    g = Boolean(h);
+    
+    if(g === true) {
+      newArr.push(h);
+    }
+    
+    
+  }
+  
+  
+  return newArr;
+}
+
+
+
+bouncer([7, "ate", "", false, 9]);
